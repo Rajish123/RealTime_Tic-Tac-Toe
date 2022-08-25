830e72ad26ecc924b656c4febb257a046e7316c6
rajish <maharjanrajish101@gmail.com>
2022-08-25 20:19:32 +0545

    game models created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    room_code = models.CharField(max_length=200)
+    game_creator = models.CharField(max_length = 100)
+    game_opponent = models.CharField(max_length=100,blank = True, null = False)
+    is_over = models.BooleanField(default=False)
